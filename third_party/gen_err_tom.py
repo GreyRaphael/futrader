6142823381b7ddccb6c39f0412d713c78268fab1
KTF41_gewei <grey@pku.edu.cn>
2025-05-08 10:49:06 +0800

    gen error dictionary

---
+++ third_party/gen_err_tom.py
@@ -0,0 +1,14 @@
+import re
+
+
+if __name__ == "__main__":
+    pat = re.compile(r'value="(\d+)" prompt="(.+?)"/>')
+    with open("./ctp/lin64/error.xml") as fin:
+        txt = fin.read()
+
+    result = pat.findall(txt)
+    print(f"record length={len(result)}")
+
+    with open("../errors.toml", "w") as fout:
+        for k, v in result:
+            fout.write(f'{k} = "{v}"\n')
