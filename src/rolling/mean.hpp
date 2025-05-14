7a722443c87357c53e8020f17ea55fcc0dd588d5
KTF41_gewei <grey@pku.edu.cn>
2025-05-14 16:02:13 +0800

    add `Meaner` & `Stder` & `Skewer`

--- src/rolling/mean.hpp
+++ src/rolling/mean.hpp
@@ -59,15 +59,42 @@ struct Sumer {
     }
 };
 
-struct Mean {
-    /* data */
+template <size_t N>
+struct Meaner {
+    Sumer<N> sumer{};
+
+    double update(double new_val) {
+        return sumer.update(new_val) / sumer.container.len();
+    }
 };
 
-struct Sum {
-    /* data */
+template <size_t N>
+struct Stder {
+    Sumer<N> sumer{};
+    Sumer<N> sq_sumer{};
+
+    double update(double new_val) {
+        auto sum = sumer.update(new_val);
+        auto sq_sum = sq_sumer.update(new_val * new_val);
+        auto variance = (sq_sum - sum * sum / N) / (N - 1);
+        return sqrt(variance);
+    }
 };
 
-struct Std {
-    /* data */
+template <size_t N>
+struct Skewer {
+    Meaner<N> meaner{};
+    Sumer<N> sq_sumer{};
+    Sumer<N> cb_sumer{};
+
+    double update(double new_val) {
+        auto mean = meaner.update(new_val);
+        auto sq_sum = sq_sumer.update(new_val * new_val);
+        auto cb_sum = cb_sumer.update(new_val * new_val * new_val);
+        auto variance = sq_sum / N - mean * mean;
+
+        return (cb_sum / N - 3.0 * mean * variance - mean * mean * mean) / std::pow(variance, 1.5);
+    }
 };
+
 }  // namespace rolling
