b1d82a847a768844a2c5128cf4ac2988b9d5b8cf
BeC9_gewei <grey@pku.edu.cn>
2025-05-18 14:28:21 +0800

    add struct size

--- src/parser/struct_parser.hpp
+++ src/parser/struct_parser.hpp
@@ -7,7 +7,7 @@
 template <typename T>
 inline void print_struct(T const *ptr) noexcept {
     auto name = ylt::reflection::get_struct_name<T>();
-    std::print("========> {}\n\t", name);
+    std::print("========> {}, size={}\n\t", name, sizeof(T));
     ylt::reflection::for_each(*ptr, [](auto &field, auto name, auto index) {
         std::print("{}:{} | ", name, field);
     });
