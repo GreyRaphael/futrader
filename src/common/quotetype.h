060d17e4e296fbc68795f8b833c4d3745a7bfa10
BeC9_gewei <grey@pku.edu.cn>
2025-05-19 02:30:17 +0800

    add `TickData`

--- src/common/quotetype.h
+++ src/common/quotetype.h
@@ -3,6 +3,8 @@
 #include <cstdint>
 
 typedef char symbol_t[32];
+typedef int volume_array_t[5];
+typedef double price_array_t[5];
 
 struct BarData {
     symbol_t symbol;
@@ -16,4 +18,29 @@ struct BarData {
     double amount;
     double oi;
     double adj;
+};
+
+struct TickData {
+    symbol_t symbol;
+    int64_t stamp;
+    double open;
+    double high;
+    double low;
+    double last;
+    double close;
+    double settle;
+    double limit_up;
+    double limit_down;
+    double preclose;
+    double presettle;
+    double volume;
+    double amouont;
+    double oi;
+    double preoi;
+    double avgprice;
+    double adj;
+    volume_array_t ask_volumes;
+    volume_array_t bid_volumes;
+    price_array_t ask_prices;
+    price_array_t bid_prices;
 };
