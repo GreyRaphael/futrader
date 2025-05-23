5cfdb830bad345322fb4e947c02d1f90f8a38dfc
KTF41_gewei <grey@pku.edu.cn>
2025-05-23 15:31:18 +0800

    add spsc test for bt_md

--- src/bt_md/bt_md_test.cc
+++ src/bt_md/bt_md_test.cc
@@ -1,19 +1,57 @@
+#include <chrono>
 #include <filesystem>
+#include <print>
 #include <string_view>
+#include <thread>
 
 #include "struct_parser.hpp"
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
 #include <bt_md.hpp>
+#include <spsc.hpp>
 
 TEST_CASE("duckdb") {
     std::string_view cfg_filename{"bt.toml"};
     REQUIRE(std::filesystem::exists(cfg_filename));
 
     BactestMdClient bt_cli{cfg_filename, [](TickData const& tick) {
-                               print_struct(&tick);
+                               // print_struct(&tick);
+                               auto tp = std::chrono::sys_time<std::chrono::milliseconds>{std::chrono::milliseconds{tick.stamp}};
+                               std::println("{} {} {}", tick.symbol, tp, tick.last);
                            }};
     bt_cli.subscribe({"MA505", "rb2505"});
     bt_cli.start();
+}
+
+using TickDataChannel = lockfree::SPSC<TickData, 1024>;
+
+TEST_CASE("duckdb_spsc") {
+    // db to SPSC is bad, as db will make SPSC full
+    std::string_view cfg_filename{"bt.toml"};
+    REQUIRE(std::filesystem::exists(cfg_filename));
+
+    TickDataChannel channel;
+
+    BactestMdClient bt_cli{cfg_filename, [&channel](TickData const& tick) {
+                               channel.push(tick);
+                           }};
+    std::jthread bt_thread{[&bt_cli] {
+        bt_cli.subscribe({"MA505", "rb2505"});
+        bt_cli.start();
+    }};
+
+    // main thread consumer
+    TickData tick{};
+    while (true) {
+        if (channel.pop(tick)) {
+            auto tp = std::chrono::sys_time<std::chrono::milliseconds>{std::chrono::milliseconds{tick.stamp}};
+            std::println("{},{},{}", tick.symbol, tp, tick.last);
+            // print_struct(&value.value());
+        } else {
+            // back off when empty
+            std::println("channel empty, retrying...");
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        }
+    }
 }
