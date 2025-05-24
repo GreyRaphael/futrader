594f20ba6fcfe2919683cdb6325d64069ba6d75b
BeC9_gewei <grey@pku.edu.cn>
2025-05-24 13:26:45 +0800

    replace nng with zmq

--- src/bt_md/bt_pub.cpp
+++ src/bt_md/bt_pub.cpp
@@ -1,23 +1,34 @@
-#include <nng/nng.h>
-#include <nng/protocol/pubsub0/pub.h>
-
 #include <bt_md.hpp>
+#include <cassert>
+#include <cstdio>
+#include <filesystem>
+#include <string>
+#include <zmq.hpp>
 
 #include "quote_type.h"
+#include "struct_parser.hpp"
 
 int main() {
     std::string_view cfg_filename = "nng.toml";
     assert(std::filesystem::exists(cfg_filename));
     auto config = NngConfig::readConfig(cfg_filename);
 
-    nng_socket pub_sock{};
-    nng_pub0_open(&pub_sock);
-    nng_listen(pub_sock, config.address.data(), nullptr, 0);  // listener=NULL; flags=0 ignored
+    zmq::context_t context{1};  // 1 I/O thread
+    zmq::socket_t publisher(context, zmq::socket_type::pub);
 
-    BactestMdClient bt_cli{config.broker_file, [&pub_sock](TickData& tick) {
-                               // nng_msleep(1);
-                               nng_send(pub_sock, &tick, sizeof(TickData), 0);  // flags=0, default for pub mode
+    // Set high-water mark for outbound messages
+    publisher.set(zmq::sockopt::sndhwm, 0);
+
+    // Bind to the same address (assuming it's something like "ipc:///tmp/your_socket")
+    publisher.bind(config.address);
+
+    BactestMdClient bt_cli{config.broker_file, [&publisher](TickData& tick) {
+                               zmq::message_t message(sizeof(TickData));
+                               std::memcpy(message.data(), &tick, sizeof(TickData));
+                               publisher.send(message, zmq::send_flags::none);
+                               //    print_struct(&tick);
                            }};
+
     bt_cli.subscribe(config.symbols);
     bt_cli.start();
 }
