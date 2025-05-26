199ab6b18d4a09671a88e91692f1833eedc07680
BeC9_gewei <grey@pku.edu.cn>
2025-05-27 01:44:20 +0800

    finish bt_md test

--- src/bt_md/bt_md_test.cc
+++ src/bt_md/bt_md_test.cc
@@ -1,4 +1,5 @@
 #include <chrono>
+#include <cstddef>
 #include <filesystem>
 #include <print>
 #include <string_view>
@@ -15,13 +16,16 @@ TEST_CASE("duckdb") {
     std::string_view cfg_filename{"bt.toml"};
     REQUIRE(std::filesystem::exists(cfg_filename));
 
-    BactestMdClient bt_cli{cfg_filename, [](TickData const& tick) {
+    size_t count = 0;
+    BactestMdClient bt_cli{cfg_filename, [&count](TickData const& tick) {
                                // print_struct(&tick);
                                auto tp = std::chrono::sys_time<std::chrono::milliseconds>{std::chrono::milliseconds{tick.stamp}};
                                std::println("{} {} {}", tick.symbol, tp, tick.last);
+                               ++count;
                            }};
     bt_cli.subscribe({"MA505", "rb2505"});
     bt_cli.start();
+    std::println("Total ticks processed: {}", count);
 }
 
 using TickDataChannel = lockfree::SPSC<TickData, 1024>;
@@ -34,7 +38,10 @@ TEST_CASE("duckdb_spsc") {
     TickDataChannel channel;
 
     BactestMdClient bt_cli{cfg_filename, [&channel](TickData const& tick) {
-                               channel.push(tick);
+                               while (!channel.push(tick)) {
+                                   std::println("channel full, cannot push tick, retrying...");
+                                   std::this_thread::sleep_for(std::chrono::milliseconds(10));
+                               }
                            }};
     std::jthread bt_thread{[&bt_cli] {
         bt_cli.subscribe({"MA505", "rb2505"});
@@ -43,14 +50,16 @@ TEST_CASE("duckdb_spsc") {
 
     // main thread consumer
     TickData tick{};
+    size_t count = 0;
     while (true) {
         if (channel.pop(tick)) {
             auto tp = std::chrono::sys_time<std::chrono::milliseconds>{std::chrono::milliseconds{tick.stamp}};
             std::println("{},{},{}", tick.symbol, tp, tick.last);
+            ++count;
             // print_struct(&value.value());
         } else {
             // back off when empty
-            std::println("channel empty, retrying...");
+            std::println("processed {} ticks, channel empty, retrying...", count);
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
     }
