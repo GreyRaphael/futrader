8e2a43c333aa9b98f0755e12fdd38248a6af6b6f
KTF41_gewei <grey@pku.edu.cn>
2025-05-27 15:02:32 +0800

    begin simple cta engine

--- src/engine/cta_engine.hpp
+++ src/engine/cta_engine.hpp
@@ -1,5 +1,21 @@
 #pragma once
 
+#include <tuple>
+
+#include "i_strategy.h"
+
+template <typename MarketSource, CtaStgConcept... T>
 struct CtaEngine {
-    
+    CtaEngine(MarketSource&& market_source, T&... cta_stgs)
+        : _market_source(std::move(market_source)), _cta_stgs(&cta_stgs...) {
+    }
+
+    void run() {
+    }
+    void stop() {
+    }
+
+   private:
+    MarketSource _market_source;
+    std::tuple<T*...> _cta_stgs;
 };
