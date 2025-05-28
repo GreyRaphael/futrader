bd2b29a41427bd7d57f5bc6ce9adfca3fd375ff7
BeC9_gewei <grey@pku.edu.cn>
2025-05-28 20:14:33 +0800

    finish CTA engine

--- src/engine/engine_cta.hpp
+++ src/engine/engine_cta.hpp
@@ -5,6 +5,7 @@
 #include <cassert>
 #include <filesystem>
 #include <string_view>
+#include <variant>
 #include <vector>
 
 #include "config_parser.h"
@@ -13,58 +14,56 @@
 #include "zmq.h"
 
 struct CtaZmqEngine {
-    CtaZmqEngine(std::string_view cfg_filename, std::vector<CtaStrategy>& cta_stgs) {
+    CtaZmqEngine(std::string_view cfg_filename) {
+        assert(std::filesystem::exists(cfg_filename));
+        auto config = ZmqConfig::readConfig(cfg_filename);
+
+        // initialize strategies
+        for (auto&& symbol : config.symbols) {
+            // Create a vector of strategies for each symbol
+            _cta_stg_map[symbol] = std::vector<CtaStrategy>{};
+            // Add strategies to the vector
+            _cta_stg_map[symbol].emplace_back(DualThrust{});
+            _cta_stg_map[symbol].emplace_back(Aberration{});
+        }
+
+        // Initialize ZeroMQ context and subscriber socket
+        _context = zmq_ctx_new();
+        _subscriber = zmq_socket(_context, ZMQ_SUB);
+        // Set high-water mark for inbound messages
+        int rcvhwm = 0;  // Adjust as needed
+        zmq_setsockopt(_subscriber, ZMQ_RCVHWM, &rcvhwm, sizeof(rcvhwm));
+        // Connect to IPC address
+        zmq_connect(_subscriber, config.address.c_str());
+        // Subscribe to topics
+        for (auto&& topic : config.symbols) {
+            zmq_setsockopt(_subscriber, ZMQ_SUBSCRIBE, topic.data(), topic.length());
+        }
+    }
+    ~CtaZmqEngine() {
+        zmq_close(_subscriber);
+        zmq_ctx_term(_context);
+    }
+
+    void start() {
+        TickData tick{};
+        while (true) {
+            // Receive messages
+            int rc = zmq_recv(_subscriber, &tick, sizeof(TickData), 0);
+            if (rc == -1) {
+                // Handle receive error
+                continue;
+            }
+
+            for (auto&& stg : _cta_stg_map[tick.symbol]) {
+                // visit strategy with the tick data
+                std::visit([&tick](auto&& strategy) { auto order = strategy.onTick(tick); }, stg);
+            }
+        }
     }
 
    private:
-    phmap::flat_hash_map<std::string_view, int> _symbol_map;
+    void* _context{nullptr};
+    void* _subscriber{nullptr};
+    phmap::flat_hash_map<std::string_view, std::vector<CtaStrategy>> _cta_stg_map;
 };
-
-// struct CtaZmqEngine {
-//     CtaZmqEngine(std::string_view cfg_filename, std::vector<CtaStrategy, typename>)
-//         : _cta_stgs(&cta_stgs...) {
-//         assert(std::filesystem::exists(cfg_filename));
-//         auto config = ZmqConfig::readConfig(cfg_filename);
-
-//         // Initialize ZeroMQ context and subscriber socket
-//         _context = zmq_ctx_new();
-//         _subscriber = zmq_socket(_context, ZMQ_SUB);
-//         // Set high-water mark for inbound messages
-//         int rcvhwm = 0;  // Adjust as needed
-//         zmq_setsockopt(_subscriber, ZMQ_RCVHWM, &rcvhwm, sizeof(rcvhwm));
-//         // Connect to IPC address
-//         zmq_connect(_subscriber, config.address.c_str());
-//         // Subscribe to topics
-//         for (auto&& topic : config.symbols) {
-//             zmq_setsockopt(_subscriber, ZMQ_SUBSCRIBE, topic.data(), topic.length());
-//         }
-//     }
-
-//     ~CtaZmqEngine() {
-//         zmq_close(_subscriber);
-//         zmq_ctx_term(_context);
-//     }
-
-//     void start() {
-//         TickData tick{};
-//         while (true) {
-//             // Receive messages
-//             int rc = zmq_recv(_subscriber, &tick, sizeof(TickData), 0);
-//             if (rc == -1) {
-//                 // Handle receive error
-//                 continue;
-//             }
-
-//             // Process tick data with each strategy
-//             std::apply([&tick](auto*... stgs) {
-//                 ((stgs->onTick(tick)), ...);
-//             },
-//                        _cta_stgs);
-//         }
-//     }
-
-//    private:
-//     std::vector<CtaStrategy> _cta_stgs;
-//     void* _context{nullptr};
-//     void* _subscriber{nullptr};
-// };
--- src/engine/engine_cta_test.cc
+++ src/engine/engine_cta_test.cc
@@ -1,12 +1,12 @@
+#include <string_view>
 #include <vector>
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 
 #include "engine_cta.hpp"
-#include "stra_cta.h"
 
 TEST_CASE("cta") {
-    std::vector<CtaStrategy> cta_stgs;
-    CtaZmqEngine engine{"zmq.toml", cta_stgs};
+    std::vector<std::string> symbols{"AAPL", "GOOGL", "MSFT"};
+    CtaZmqEngine engine{"zmq.toml"};
 }
